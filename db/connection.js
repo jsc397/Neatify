9eed8858106a4c615241a98e820db490f63d0418
“Stephanie <stephanie.techarmour@gmail.com>
2018-08-03 14:00:32 -0400

    updated mongo database for screencast

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,9 @@
 // mongoose.connect('mongodb://localhost/db_name')
 const mongoose = require("mongoose")
-// mongoose.connect("mongodb://localhost:27017/neatifydb", { useNewUrlParser: true }) //neatigydb is my database name
 if (process.env.NODE_ENV == "production") {
     mongoose.connect(process.env.MLAB_URL)
 } else {
-    mongoose.connect("mongodb://localhost/neatifydb");
+    mongoose.connect("mongodb://localhost:27017/neatifydb", { useNewUrlParser: true }) //neatifydb is my database name;
 }
 mongoose.Promise = Promise
 module.exports = mongoose
--- db/seed.js
+++ db/seed.js
@@ -1,27 +1,27 @@
 const User = require("../models/User");
 const { Solution } = require("../models/Solution")
-// const { Problem } = require("../models/Problem");
+const { Problem } = require("../models/Problem");
 
-// Problem.find({}).remove(() => {
-//     Problem.create({
-//         paragraph: "TEDSTING DFSDJFKDJFKDJ"
-//     }).then(problems => {
-//         problems.push();
-//     }),
-//     Problem.create({
-//         paragraph: "THIS IS TEST NUMBER TWO"
-//     }).then(problems => {
-//         problems.push()
-//     }),
-//     Problem.create({
-//         paragraph: "This is another frikken test"
-//     }).then(problems => {
-//             problems.push()
+Problem.find({}).remove(() => {
+    Problem.create({
+        paragraph: "If you have a sentence with an object in it -- basically a noun that receives the action -- passive voice can happen to you. Passive happens when the object of a sentence is put at the beginning of a sentence instead of at the end. With passive voice, your writing comes across as sounding weak and unclear."
+    }).then(problems => {
+        problems.push();
+    }),
+    Problem.create({
+        paragraph: "THIS IS TEST NUMBER TWO"
+    }).then(problems => {
+        problems.push()
+    }),
+    Problem.create({
+        paragraph: "This is another frikken test"
+    }).then(problems => {
+            problems.push()
     
-//     }).then(() => {
-//         problem.save(err => console.log(err))
-//     })
-// });
+    }).then(() => {
+        problem.save(err => console.log(err))
+    })
+});
 
 User.find({}).remove(() => {
     Solution.find({}).remove(() => {
